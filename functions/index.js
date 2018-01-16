851afffe3b90d3a356ecc6fe5fe5377ad30799ed
Arkadiusz <nedar3@gmail.com>
2018-01-16 21:48:08 +0100

    add html template to sendNotification()

--- functions/index.js
+++ functions/index.js
@@ -10,6 +10,10 @@ admin.initializeApp(functions.config().firebase);
 const gmailEmail = functions.config().gmail.email;
 const gmailPassword = functions.config().gmail.password;
 
+
+const EmailTemplate = require('email-templates').EmailTemplate;
+
+
 const mailTransport = nodemailer.createTransport( {
   host: 'smtp.gmail.com',
         port: 587,
@@ -69,6 +73,29 @@ exports.sendReply = functions.firestore.document('posts/{userID}').onCreate(even
       to: email
     };
   
+    mailOptions.html = 
+    
+`
+
+<div style="width: 70%; text-align: center; padding: 2rem; margin: 0 auto; color: white; background-color: #34495e; border-radius: 10px;">
+<h2 style="font-size: 1.8rem">Dziękuję za wiadomość, ${email.split('@')[0]}!</h2>
+<p style="font-size: 1.2rem">Z pewnością odpowiem na nią w najbliższym czasie. Cieszę się, że moje portfolio przypadło Ci do gustu!</p>
+
+
+<p style="font-size: .9rem; margin: 2.5rem 0 .5rem 0;">Z wyrazami szacunku,</p>
+<p style="font-size: 1.1rem; margin-bottom: .5rem 0;">Arkadiusz Mierzejewski</p>
+<img src='https://i.imgur.com/2YnLjQB.png' style="margin-top: 2rem; border-radius: 10px;"/>
+</div>
+
+
+
+
+
+
+
+
+`
+
     mailOptions.subject = `Webarq - Dziękuję za wiadomość!`;
     mailOptions.text = 
 
@@ -78,6 +105,8 @@ exports.sendReply = functions.firestore.document('posts/{userID}').onCreate(even
 Z wyrazami szacunku,
 
 Arkadiusz Mierzejewski`;
+
+  
   
   
     return mailTransport.sendMail(mailOptions).then(() => {
